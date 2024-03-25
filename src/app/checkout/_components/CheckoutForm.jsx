852c680625038dacf44107314c3dc5ba9ce79018
mohamed hassaan <mmhassaan3@gmail.com>
2024-03-26 00:54:09 +0400

    deploy strapi on server

--- src/app/checkout/_components/CheckoutForm.jsx
+++ src/app/checkout/_components/CheckoutForm.jsx
@@ -1,5 +1,100 @@
 "use client";
+import React from "react";
 import {
+  PaymentElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js";
+
+export default function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+
+  const [message, setMessage] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(process.env.STRIP_SECRET_KEY);
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
+    });
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occurred.");
+    }
+
+    setIsLoading(false);
+  };
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
+  );
+}
+/* import {
   useStripe,
   useElements,
   PaymentElement,
@@ -8,19 +103,25 @@ import {
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     if (!stripe || !elements) {
       // Stripe.js hasn't yet loaded.
       // Make sure to disable form submission until Stripe.js has loaded.
       return;
     }
+    const res = await fetch('api/create-intent', {
+      method: "POST",
+      body: JSON.stringify({
+        amount: 10
+      })
+    })
+    clientSecret = await res.json();
 
     const result = await stripe.confirmPayment({
       //`Elements` instance that was used to create the Payment Element
+      clientSecret,
       elements,
       confirmParams: {
         return_url: "/",
@@ -28,7 +129,6 @@ const CheckoutForm = () => {
     });
 
     if (result.error) {
-      // Show error to your customer (for example, payment details incomplete)
       console.log(result.error.message);
     } else {
       // Your customer will be redirected to your `return_url`. For some payment
@@ -37,7 +137,7 @@ const CheckoutForm = () => {
     }
   };
   return (
-    <form className="w-[60%] text-center m-auto h-screen">
+    <form onSubmit={handleSubmit} className="w-[60%] text-center m-auto h-screen">
       <PaymentElement />
       <button>Submit</button>
     </form>
@@ -45,3 +145,4 @@ const CheckoutForm = () => {
 };
 
 export default CheckoutForm;
+ */
