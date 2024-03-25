56085df54b66a82b81eaf4fb3036be1c66d73a3f
mohamed hassaan <mmhassaan3@gmail.com>
2024-03-25 23:03:09 +0400

    deploy strapi on server

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = "http://localhost:1337/api/";
+const URL_API = "https://courses-online-strapi.onrender.com/api/";
 const API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 export default axios.create({
